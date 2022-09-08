0f2449619d3fae187fc93c6e1c6f9cb9ee883119
Esancal <esteban.ca2000@outlook.cl>
2022-09-08 16:59:45 -0300

    Funcionalidad a contacto añadida (no terminado)

--- js/contacto.js
+++ js/contacto.js
@@ -1,40 +1,60 @@
-window.addEventListener("DOMContentLoaded", () => {
-  // Con el evento DOMContentLoaded me aseguro que todas las
-  // etiquetas HTML fueron cargadas y procesadas por el browser
-  // Info en https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+// window.addEventListener("DOMContentLoaded", () => {
+//   // Con el evento DOMContentLoaded me aseguro que todas las
+//   // etiquetas HTML fueron cargadas y procesadas por el browser
+//   // Info en https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 
-  /* 
-  Sintaxis de variables:
-  let nombreDeVariable = valor;
-  */
+//   let razonContacto = getRazonContacto;
 
-  let razonContacto = getRazonContacto;
+//   let btnEnviar = document.getElementById("btnEnviar");
 
-  let btnEnviar = document.getElementById("btnEnviar");
+//   btnEnviar.addEventListener("click", () => {
+//     let nombre = document.getElementById("nombre").value;
+//     let correo = document.getElementById("correo").value;
 
-  btnEnviar.addEventListener("click", () => {
-    let nombre = document.getElementById("nombre").value;
-    let correo = document.getElementById("correo").value;
+//     console.log("Nombre: " + nombre);
+//     console.log("Correo: " + correo);
+//   });
 
-    console.log("Nombre: " + nombre);
-    console.log("Correo: " + correo);
+//   console.log("Evento DOMContentLoaded");
+// });
+
+// // Con esta funcion podremos ver que alternativa esta seleccionada
+// function getRazonContacto() {
+//   let entradaRazon = document.querySelector("input[name='razon']:checked");
+//   if(entradaRazon == null){
+//     mostrarError("Debe seleccionar una opcion");
+//     return false;
+//   }
+// }
+
+// //
+// function mostrarError () {
+//   console.error(mensajeDeError);
+// }
+$(document).ready(function () {
+  // similar a querySelectorAll()
+  // param es un string con selector CSS
+  $("#cabecera").load("parts/header.html");
+  $("#pie-pagina").load("parts/footer.html");
+
+  $("#btnEnviar").on("click", function () {
+    const nombre = $("#nombre").val();
+    const email = $("#correo").val();
+    const titulo = $("#titulo").val();
+
+    const baseUrl = "https://portafolio-f3200-default-rtdb.firebaseio.com";
+    const url = baseUrl + "/contacto.json";
+    $.ajax(url, {
+      method: "POST",
+      data: JSON.stringify({ nombre, email, titulo }),
+    })
+      .done(function (d) {
+        alert("Se insertó su mensaje con ID: " + d.name);
+      })
+      .fail(function () {
+        alert(
+          "Su mensaje no pudo ser guardado, por favor contacte al administrador!!!"
+        );
+      });
   });
-
-  console.log("Evento DOMContentLoaded");
 });
-
-// Con esta funcion podremos ver que alternativa esta seleccionada
-function getRazonContacto() {
-  let entradaRazon = document.querySelector("input[name='razon']:checked");
-  if(entradaRazon == null){
-    mostrarError("Debe seleccionar una opcion");
-    return false;
-  }
-}
-
-//
-function mostrarError () {
-  console.error(mensajeDeError);
-}
-
-console.log("JS del head");

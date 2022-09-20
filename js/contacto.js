8d31cecd25d34eaceae1145ef209c79175c9450b
Esancal <esteban.ca2000@outlook.cl>
2022-09-19 21:22:57 -0300

    Base para comprobacion mediante js

--- js/contacto.js
+++ js/contacto.js
@@ -1,36 +1,3 @@
-// window.addEventListener("DOMContentLoaded", () => {
-//   // Con el evento DOMContentLoaded me aseguro que todas las
-//   // etiquetas HTML fueron cargadas y procesadas por el browser
-//   // Info en https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-
-//   let razonContacto = getRazonContacto;
-
-//   let btnEnviar = document.getElementById("btnEnviar");
-
-//   btnEnviar.addEventListener("click", () => {
-//     let nombre = document.getElementById("nombre").value;
-//     let correo = document.getElementById("correo").value;
-
-//     console.log("Nombre: " + nombre);
-//     console.log("Correo: " + correo);
-//   });
-
-//   console.log("Evento DOMContentLoaded");
-// });
-
-// // Con esta funcion podremos ver que alternativa esta seleccionada
-// function getRazonContacto() {
-//   let entradaRazon = document.querySelector("input[name='razon']:checked");
-//   if(entradaRazon == null){
-//     mostrarError("Debe seleccionar una opcion");
-//     return false;
-//   }
-// }
-
-// //
-// function mostrarError () {
-//   console.error(mensajeDeError);
-// }
 $(document).ready(function () {
   // similar a querySelectorAll()
   // param es un string con selector CSS
@@ -38,26 +5,66 @@ $(document).ready(function () {
   $("#pie-pagina").load("parts/footer.html");
 
   $("#btnEnviar").on("click", function () {
-    const nombre = $("#nombre").val();
-    const email = $("#correo").val();
-    const titulo = $("#titulo").val();
-    const razon = $("input[name='razon']:checked").val();
-    const mensaje = $("#mensaje").val();
-    const fecha_mensaje = new Date;
+    const nombre = itsBlank($("#nombre").val(), "Nombre");
+    const email = getCorreo($("#correo").val());
+    const titulo = itsBlank($("#titulo").val(), "Titulo");
+    const razon = getRazon($("input[name='razon']:checked").val());
+    const mensaje = itsBlank($("#mensaje").val(), "Mensaje");
+    const fecha_mensaje = new Date();
 
     const baseUrl = "https://portafolio-f3200-default-rtdb.firebaseio.com";
     const url = baseUrl + "/contacto.json";
     $.ajax(url, {
       method: "POST",
-      data: JSON.stringify({ nombre, email, titulo, razon, mensaje, fecha_mensaje }),
+      data: JSON.stringify({
+        nombre,
+        email,
+        titulo,
+        razon,
+        mensaje,
+        fecha_mensaje,
+      }),
     })
       .done(function (d) {
         alert("Se insertó su mensaje con ID: " + d.name);
       })
       .fail(function () {
-        alert(
-          "Su mensaje no pudo ser guardado, por favor intente mas tarde"
-        );
+        alert("Su mensaje no pudo ser guardado, por favor intente mas tarde");
       });
   });
+
+  function itsBlank(value, name) {
+    if (value == "") {
+      console.log(name + ": El campo esta vacio");
+    } else {
+      console.log(name + ": El campo no esta vacio");
+      if (name == "Nombre" && value.length > 100) {
+        console.log("Nombre: Cantidad de caracteres superior a 100");
+      }
+      if (name == "Titulo" && value.length > 80) {
+        console.log("Titulo: Cantidad de caracteres superior a 80");
+      }
+      if (name == "Mensaje" && value.length > 1300) {
+        console.log("Mensaje: Cantidad de caracteres superior a 1300");
+      }
+    }
+  }
+
+  function getCorreo(correo) {
+    emailRegex =
+      /^(?:[^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*|"[^\n"]+")@(?:[^<>()[\].,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,63}$/i;
+
+    if (emailRegex.test(correo)) {
+      console.log("Correo: Formato correcto");
+    } else {
+      console.log("Correo: Formato incorrecto");
+    }
+  }
+
+  function getRazon(razon) {
+    // Si no seleccionan razon = undefined
+    if (razon == undefined) {
+      console.log("Razon: undefined");
+    }
+  }
 });
